b0db9fe8ee9d13db14aaffdfa1354813f18caaba
vientran199 <vientran1999@gmail.com>
2022-10-25 17:44:04 +0700

    fake login

--- src/features/auth/authSaga.ts
+++ src/features/auth/authSaga.ts
@@ -1,29 +1,38 @@
 import { PayloadAction } from "@reduxjs/toolkit";
-import { delay, fork, take,call } from "redux-saga/effects";
+import { delay, fork, take, call, put } from "redux-saga/effects";
 import { authActions, LoginPayload } from "./authSlice";
 
-function* hanleLogin(payload: LoginPayload){
-    console.log('login',payload)
-    yield delay(500)
-    localStorage.setItem('acces_token',"áds")
+function* hanleLogin(payload: LoginPayload) {
+    try {
+        console.log('login', payload)
+        yield delay(500)
+        localStorage.setItem('acces_token', "áds")
+        yield put(authActions.loginSuccess({
+            id: '1',
+            name: 'vien'
+        }))
+    } catch (error) {
+        yield put(authActions.loginFail('error'))
+    }
+
 }
-function* handleLogout(){
+function* handleLogout() {
     console.log('logout')
     localStorage.removeItem('access_token')
 }
-function* watchLoginFlow(){
-    while(true){ //để chạy logout, sẽ quay lại chờ login
+function* watchLoginFlow() {
+    while (true) { //để chạy logout, sẽ quay lại chờ login
         const isLoggedIn = localStorage.getItem('access_token')
-        if(isLoggedIn){
-            const action:PayloadAction<LoginPayload> = yield take(authActions.login.type) //Chờ action login mới chạy tiếp
-            yield fork(hanleLogin,action.payload)
+        if (isLoggedIn) {
+            const action: PayloadAction<LoginPayload> = yield take(authActions.login.type) //Chờ action login mới chạy tiếp
+            yield fork(hanleLogin, action.payload)
         }
-        
+
         yield take(authActions.logout.type) //chờ action logout mới chạy tiếp
         yield call(handleLogout) //fork: sẽ tự chạy tiếp không chờ cho xong công việc, call: chờ xong công việc
     }
 }
-export function* authSaga(){
+export function* authSaga() {
     console.log('authSaga')
     yield fork(watchLoginFlow)
 }
