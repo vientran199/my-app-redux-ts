8dd393d34ee84c6fc3e57c5fc7cc8bf02e878dbc
vientran199 <vientran1999@gmail.com>
2022-10-27 21:24:21 +0700

    nav in saga

--- src/components/Layout/Admin.tsx
+++ src/components/Layout/Admin.tsx
@@ -1,3 +1,5 @@
+import { useAppDispatch } from 'app/hooks'
+import { authActions } from 'features/auth/authSlice'
 import * as React from 'react'
 
 export interface AdminLayoutProps{
@@ -5,9 +7,14 @@ export interface AdminLayoutProps{
 }
 
 export const AdminLayout = ()=>{
+    const dispatch = useAppDispatch()
+    const handleLogout = () =>{
+        dispatch(authActions.logout())
+    }
     return (
         <div>
             addmin
+            <button onClick={handleLogout}>Logout</button>
         </div>
     )
 }
--- src/features/auth/authSaga.ts
+++ src/features/auth/authSaga.ts
@@ -29,15 +29,21 @@ function* watchLoginFlow() {
     while (true) { //để chạy logout, sẽ quay lại chờ login
         const isLoggedIn = localStorage.getItem('access_token')
         if (!isLoggedIn) {
-            const action: PayloadAction<LoginPayload> = yield take(authActions.login.type) //Chờ action login mới chạy tiếp
+            const action: PayloadAction<LoginPayload> = yield take(authActions.login.type) //Chờ đến khi nhận được action login mới chạy tiếp
             yield fork(hanleLogin, action.payload)
         }
 
-        yield take(authActions.logout.type) //chờ action logout mới chạy tiếp
+        yield take(authActions.logout.type) //chờ đến khi nhận được action logout mới chạy tiếp
         yield call(handleLogout) //fork: sẽ tự chạy tiếp không chờ cho xong công việc, call: chờ xong công việc
     }
 }
+function* checkLogin() {
+    const auth = localStorage.getItem('access_token')
+    if(auth) yield put(push('/admin'))
+}
 export function* authSaga() {
     console.log('authSaga')
+
+    yield call(checkLogin)
     yield fork(watchLoginFlow)
 }
